304d439993132a1cf9c29a18ceb4e0fe427590e9
Charith Ellawala <charith.ellawala@gmail.com>
2020-06-22 11:54:50 +0100

    Use Piston event loop

--- README.md
+++ README.md
@@ -1,6 +1,39 @@
 # chip8
+
 Yet another Chip-8 emulator written in Rust.
 
 ```
-RUST_LOG=info cargo run roms/TANK
+RUST_LOG=info cargo run roms/BLITZ
 ```
+
+Presss <kbd>Esc</kbd> to exit the emulator.
+
+ROMs obtained from https://github.com/JamesGriffin/CHIP-8-Emulator and https://github.com/corax89/chip8-test-rom.
+
+
+Keyboard
+--------
+
+Chip-8 Layout:
+
+| `1` | `2` | `3` | `C` |
+| `4` | `5` | `6` | `D` |
+| `7` | `8` | `9` | `E` |
+| `A` | `0` | `B` | `F` |
+
+Equivalent mapping to QWERTY:
+
+| <kbd>1</kbd> | <kbd>2</kbd> | <kbd>3</kbd> | <kbd>4</kbd> |
+| <kbd>Q</kbd> | <kbd>W</kbd> | <kbd>E</kbd> | <kbd>R</kbd> |
+| <kbd>A</kbd> | <kbd>S</kbd> | <kbd>D</kbd> | <kbd>F</kbd> |
+| <kbd>Z</kbd> | <kbd>X</kbd> | <kbd>C</kbd> | <kbd>V</kbd> |
+
+
+Screenshots
+-----------
+
+![Blitz](screenshots/blitz.png)
+
+![Space Invaders](screenshots/space_invaders.png)
+
+![Test ROM](screenshots/test_rom.png)
--- src/emulator/ui/gui/mod.rs
+++ src/emulator/ui/gui/mod.rs
@@ -1,32 +1,45 @@
 use crate::emulator;
 use crate::emulator::common::Result;
+use crate::emulator::display;
 use piston_window::*;
 
-const TICK_RATE: u64 = 15;
+const CLOCK_SPEED: f64 = 60.0f64;
 
 pub fn start_loop(emu: &mut emulator::Emulator) -> Result<()> {
     let mut window: PistonWindow = WindowSettings::new("Chip-8", [640, 320])
         .exit_on_esc(true)
-        .samples(4)
+        .vsync(true)
         .resizable(false)
         .build()
         .unwrap();
 
-    window.set_ups(TICK_RATE);
-    window.set_ups_reset(0);
-    window.set_max_fps(60);
+    window.set_ups(CLOCK_SPEED as u64);
+    window.set_max_fps(CLOCK_SPEED as u64);
     window.set_swap_buffers(true);
     window.set_lazy(false);
 
-    while let Some(e) = window.next() {
-        to_input(&e).iter().for_each(|i| emu.send_input(*i));
+    let mut pixels = display::Pixels::default();
 
-        let step = emu.step();
-        match step {
-            Ok(Some(emulator::Step::Exit)) => {
-                return Ok(());
+    while let Some(e) = window.next() {
+        match e {
+            Event::Loop(Loop::Update(args)) => {
+                let num_steps = (args.dt * CLOCK_SPEED).round() as usize;
+                for _i in 0..num_steps {
+                    match emu.step() {
+                        Ok(Some(emulator::Step::Exit)) => {
+                            return Ok(());
+                        }
+                        Ok(Some(emulator::Step::Draw(p))) => {
+                            pixels = p.clone();
+                        }
+                        Ok(_) => {}
+                        Err(err) => {
+                            return Err(err);
+                        }
+                    };
+                }
             }
-            Ok(Some(emulator::Step::Draw(pixels))) => {
+            Event::Loop(Loop::Render(_)) => {
                 window.draw_2d(&e, |c, g, _| {
                     clear([0.0, 0.0, 0.0, 0.0], g);
                     pixels.iter().for_each(|p| {
@@ -39,38 +52,37 @@ pub fn start_loop(emu: &mut emulator::Emulator) -> Result<()> {
                     });
                 });
             }
-            Ok(_) => {}
-            Err(err) => {
-                return Err(err);
+            Event::Input(Input::Button(args), _) => {
+                if let Button::Keyboard(key) = args.button {
+                    let input = match key {
+                        Key::D1 => Some(emulator::Input::Key1),
+                        Key::D2 => Some(emulator::Input::Key2),
+                        Key::D3 => Some(emulator::Input::Key3),
+                        Key::D4 => Some(emulator::Input::KeyC),
+                        Key::Q => Some(emulator::Input::Key4),
+                        Key::W => Some(emulator::Input::Key5),
+                        Key::E => Some(emulator::Input::Key6),
+                        Key::R => Some(emulator::Input::KeyD),
+                        Key::A => Some(emulator::Input::Key7),
+                        Key::S => Some(emulator::Input::Key8),
+                        Key::D => Some(emulator::Input::Key9),
+                        Key::F => Some(emulator::Input::KeyE),
+                        Key::Z => Some(emulator::Input::KeyA),
+                        Key::X => Some(emulator::Input::Key0),
+                        Key::C => Some(emulator::Input::KeyB),
+                        Key::V => Some(emulator::Input::KeyF),
+                        _ => None,
+                    };
+
+                    input.iter().for_each(|i| match args.state {
+                        ButtonState::Press => emu.key_press(*i),
+                        ButtonState::Release => emu.key_release(*i),
+                    });
+                }
             }
-        };
+            _ => {}
+        }
     }
 
     Ok(())
-}
-
-fn to_input(evt: &Event) -> Option<emulator::Input> {
-    if let Some(Button::Keyboard(key)) = evt.press_args() {
-        match key {
-            Key::D1 => Some(emulator::Input::Key1),
-            Key::D2 => Some(emulator::Input::Key2),
-            Key::D3 => Some(emulator::Input::Key3),
-            Key::D4 => Some(emulator::Input::KeyC),
-            Key::Q => Some(emulator::Input::Key4),
-            Key::W => Some(emulator::Input::Key5),
-            Key::E => Some(emulator::Input::Key6),
-            Key::R => Some(emulator::Input::KeyD),
-            Key::A => Some(emulator::Input::Key7),
-            Key::S => Some(emulator::Input::Key8),
-            Key::D => Some(emulator::Input::Key9),
-            Key::F => Some(emulator::Input::KeyE),
-            Key::Z => Some(emulator::Input::KeyA),
-            Key::X => Some(emulator::Input::Key0),
-            Key::C => Some(emulator::Input::KeyB),
-            Key::V => Some(emulator::Input::KeyF),
-            _ => None,
-        }
-    } else {
-        None
-    }
 }
